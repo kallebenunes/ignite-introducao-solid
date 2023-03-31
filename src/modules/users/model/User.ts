c64ddc384e1286f7d0ac68394f7e90c02d9ca847
Kallebe Nunes <kallebe.nunes@trinto.com.br>
2023-03-31 01:50:23 -0300

    feat: create class User

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -2,6 +2,16 @@ import { v4 as uuidV4 } from "uuid";
 
 class User {
   // Complete aqui
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+  id: string;
+  admin: boolean;
+  constructor() {
+    this.id = uuidV4();
+    this.admin = false;
+  }
 }
 
 export { User };
